9fc5ee975be5bdc784fb2c011d1025070da67cec
Max <maxstill2006@gmail.com>
2024-11-19 19:43:08 +0200

    +

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace Classes_lab
     {
         static void Main(string[] args)
         {
+            Multiplier multiplier = new Multiplier(lowerBound: 1, upperBound: 6);
+            Console.WriteLine(multiplier.ToString());
         }
     }
 }
