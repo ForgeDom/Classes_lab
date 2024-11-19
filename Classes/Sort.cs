27fd08f0b58df8d91cd45e68653c78e39a4f50a4
Max <maxstill2006@gmail.com>
2024-11-19 20:41:57 +0200

    +

---
+++ Classes/Sort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_lab.Classes
+{
+    internal class Sort
+    {
+        private int[] arr;
+        
+        public Sort(int[] inputArr)
+        {
+            arr = inputArr;
+        }
+
+        public void SortArray(bool ascending)
+        {
+            Array.Sort(arr);
+            if (!ascending)
+            {
+                Array.Reverse(arr);
+            }
+        }
+
+        // Метод для виведення масиву
+        public void PrintArray()
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,38 @@ namespace Classes_lab
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the size of the array:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+
+            Console.WriteLine("Enter the range for random numbers:");
+            Console.Write("Lower bound: ");
+            int lowerBound = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Upper bound: ");
+            int upperBound = Convert.ToInt32(Console.ReadLine());
+
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rand.Next(lowerBound, upperBound + 1); 
+            }
+
+            Console.WriteLine("Generated array:");
+            Console.WriteLine(string.Join(" ", arr));
+
+            Console.WriteLine("Choose sorting order: 1 for Ascending, 2 for Descending:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            bool ascending = choice == 1;
+
+            Sort sorter = new Sort(arr);
+
+            Console.WriteLine("Array before sorting:");
+            sorter.PrintArray();
+
+            sorter.SortArray(ascending);
+
+            Console.WriteLine("Array after sorting:");
+            sorter.PrintArray();
         }
     }
 }
