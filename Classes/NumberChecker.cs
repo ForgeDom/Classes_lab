aae87ca8782d43c7729578b511a746ffe56f6cf0
Max <maxstill2006@gmail.com>
2024-11-19 19:52:53 +0200

    +

---
+++ Classes/NumberChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_lab.Classes
+{
+    internal class NumberChecker
+    {
+        public static bool IsFibonacci(int number)
+        {
+            if (number < 0)
+            {
+                return false;
+            }
+
+            int a = 0;
+            int b = 1;
+
+            if (number == a || number == b)
+                return true;
+
+            int nextFib = a + b;
+
+            while (nextFib <= number)
+            {
+                if (nextFib == number)
+                    return true;
+
+                a = b;
+                b = nextFib;
+                nextFib = a + b;
+            }
+
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ namespace Classes_lab
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (NumberChecker.IsFibonacci(number))
+            {
+                Console.WriteLine($"{number} is a Fibonacci number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a Fibonacci number.");
+            }
         }
     }
 }
