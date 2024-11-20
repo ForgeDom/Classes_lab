5a7a2468e9832c057f62f13535f758b60a6cd840
Max <maxstill2006@gmail.com>
2024-11-20 15:09:00 +0200

    +

---
+++ Classes/PalindromeChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_lab.Classes
+{
+    internal class PalindromeChecker
+    {
+        public static bool IsPalindrome(int number)
+        {
+            string numberStr = number.ToString();             
+            string reversedStr = new string(numberStr.Reverse().ToArray()); 
+            return numberStr == reversedStr;                   
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@ namespace Classes_lab
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Enter a number to check if it's a palindrome:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool result = PalindromeChecker.IsPalindrome(number);
+
+            if (result)
+            {
+                Console.WriteLine($"{number} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a palindrome.");
+            }
+        }   
     }
 }
