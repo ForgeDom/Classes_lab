7de26543116fbd163be57dd79fe955e75e6f3144
ForgeDom <maxstill2006@gmail.com>
2025-01-10 21:09:54 +0200

    +

---
+++ Classes/FilterUtil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_lab.Classes
+{
+    internal class FilterUtil
+    {
+        public static int[] FilterArray(int[] originalArray, int[] filterArray)
+        {
+            // Масив для зберігання результату
+            int[] tempArray = new int[originalArray.Length];
+            int index = 0;
+
+            // Перебір елементів оригінального масиву
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                bool toFilter = false;
+
+                // Перевірка, чи є елемент у масиві для фільтрації
+                for (int j = 0; j < filterArray.Length; j++)
+                {
+                    if (originalArray[i] == filterArray[j])
+                    {
+                        toFilter = true;
+                        break;
+                    }
+                }
+
+                // Якщо елемент не знайдено в масиві для фільтрації, додаємо його до результату
+                if (!toFilter)
+                {
+                    tempArray[index++] = originalArray[i];
+                }
+            }
+
+            // Створюємо масив потрібного розміру
+            int[] resultArray = new int[index];
+            Array.Copy(tempArray, resultArray, index);
+
+            return resultArray;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,24 @@ namespace Classes_lab
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the original array elements separated by space:");
+            string[] originalInput = Console.ReadLine().Split(' ');
+            int[] originalArray = Array.ConvertAll(originalInput, int.Parse);
+
+            // Введення масиву для фільтрації
+            Console.WriteLine("Enter the filter array elements separated by space:");
+            string[] filterInput = Console.ReadLine().Split(' ');
+            int[] filterArray = Array.ConvertAll(filterInput, int.Parse);
+
+            // Виклик методу
+            int[] filteredArray = FilterUtil.FilterArray(originalArray, filterArray);
+
+            // Виведення результату
+            Console.WriteLine("Filtered array:");
+            foreach (int item in filteredArray)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
