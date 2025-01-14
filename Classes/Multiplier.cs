18c39e8660affdd312eba3607b7600ab86b4b459
ForgeDom <maxstill2006@gmail.com>
2025-01-14 15:10:50 +0200

    +

--- Classes/Multiplier.cs
+++ Classes/Multiplier.cs
@@ -4,11 +4,44 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Classes_lab.Classes
 {
-    internal class Multiplier
+    internal class Website
     {
-        
+        private string _name;
+        private string _url;
+        private string _description;
+        private string _ipAddress;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Url
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public string IpAddress
+        {
+            get { return _ipAddress; }
+            set { _ipAddress = value; }
+        }
+        public void DisplayData()
+        {
+            Console.WriteLine("\nІнформація про сайт:");
+            Console.WriteLine($"Назва: {_name}");
+            Console.WriteLine($"URL: {_url}");
+            Console.WriteLine($"Опис: {_description}");
+            Console.WriteLine($"IP-адреса: {_ipAddress}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ namespace Classes_lab
     {
         static void Main(string[] args)
         {
+            Website website = new Website();
+            Console.WriteLine("Введіть назву сайту:");
+            website.Name = Console.ReadLine();
+            Console.WriteLine("Введіть URL сайту:");
+            website.Url = Console.ReadLine();
+            Console.WriteLine("Введіть опис сайту:");
+            website.Description = Console.ReadLine();
+            Console.WriteLine("Введіть IP-адресу сайту:");
+            website.IpAddress = Console.ReadLine();
+            website.DisplayData();
+
         }
     }
 }
